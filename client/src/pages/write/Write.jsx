0602a3ebe70b5f649e5811a637ece63953646326
Olivia Park <oliviaypark113@gmail.com>
2021-07-23 16:30:21 -0400

    updated so far

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -2,6 +2,7 @@ import "./write.css"
 import { useState ,useContext} from "react"
 import axios from "axios"
 import {Context} from "../../context/Context"
+// import {Image} from "cloudinary-react"
 
 export default function Write() {
 
@@ -10,53 +11,71 @@ export default function Write() {
   const [file, setFile] = useState(null)
   const {user} = useContext(Context)
 
-  const handleSubmit = async (e)=>{
 
+  const handleSubmit = async(e)=>{
+    console.log("submitting")
     e.preventDefault();
+    console.log(e.target)
     const newPost ={
       username: user.username,
       title,
       desc, 
     };
+    if(file) {
 
-    if(file){
       const data = new FormData();
-      const filename= Date.now() + file.name;
-      data.append("name",filename)
-      data.append("file",file)
+      const filename=file.name; 
+      // const filename= Date.now() + file.name; 
+      data.append("name", filename)
+      data.append("file", file)
       newPost.photo = filename;
 
+
       try{
-        await axios.post("/api/upload", data);
-      }
-      catch(err){
-          console.log(err)
-      }
-    }
-    try{
-      const res = await axios.post("/api/posts", newPost);
-      window.location.replace("/post/"+res.data._id)
+        const res = await axios.post("/api/upload", data); 
+        console.log(res.data)      
+       }
+       catch(err)
+       {
+        console.log(err)
+       }
     }
 
+    try{
+      const res = await axios.post("/api/posts", newPost);
+      console.log(res.data)   
+      window.location.replace("/post/"+ res.data._id)
+    }
     catch(err){
       console.log(err)
   }
-    
 
   }
+
+ 
+
+
+
   return (
-    <div className="write"onSubmit={handleSubmit}>
+    <div className="write" >
 
       {file && (<img className="writeImg" src={URL.createObjectURL(file)} alt=""/>)}
- 
-      <form className="writeForm">
+
+      <form className="writeForm" 
+      onSubmit={handleSubmit} 
+    >
           <div className="writeFormGroup">
               <label htmlFor="fileInput">
               <i className=" writeIcon fas fa-plus"></i>
               </label>
-          <input type="file" id="fileInput" 
+          <input 
+          type="file" 
+          id="fileInput" 
+          name="image"
           style={{display:"none"}}
-          onChange={e=>setFile(e.target.files[0])}/>
+          onChange={e=>setFile(e.target.files[0])}
+      
+          />
           <input type="text" 
           placeholder="Title" 
           className="writeInput" 
--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
 const multer = require("multer");
-const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+const {cloudinary} = require("./utils/cloudinary")
 
-const Post = require("./models/Post")
-
-const s3 = new aws.S3({
-  accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY,
-  secretAccessKey: process.env.REACT_APP_S3_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_S3_BUCKET_REGION,
-});
 
 dotenv.config();
 
@@ -35,6 +27,9 @@ mongoose.connect(
  
 
 // //middleware
+// app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("common"));
@@ -53,51 +48,23 @@ if (process.env.NODE_ENV === "production") {
 
 }
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   try {
-//     return res.status(200).json("File uploded successfully");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "direct-upload-s3-bucket",
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      console.log(req)
-      cb(null, req.body.name)
-    }
-  })
-})
-
-
- app.post("/api/upload", upload.single("file"), (req, res) => {
-  console.log(req.body, req.file)
-try {
-
-  return res.status(200).json("File Uploaded");
-
-} 
-catch (error) {
-  console.error(error);
-}
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
